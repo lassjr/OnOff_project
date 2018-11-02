761c45d2c2331dda4a71ad1db5b2d7b472c14f30
Giovanni Lasagna <lasagna.giovanni@gmail.com>
2018-11-02 17:17:30 +0100

    add function to send pinout of each version of raspberry

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,8 +11,12 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
-	"github.com/stianeikeland/go-rpio"
+	rpio "github.com/stianeikeland/go-rpio"
 )
+
+type Pinout struct {
+	Pin []int `json:"pin"`
+}
 
 var raspberrySupportedVersion = map[string][]int{
 	"v1": []int{2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 22, 23, 24, 25, 27},
@@ -35,6 +40,20 @@ func check(key string, pin int) bool {
 		}
 	}
 	return false
+}
+
+// Version return
+func Version(w http.ResponseWriter, req *http.Request) {
+	paramVersion := req.URL.Query().Get("ver")
+	keys := reflect.ValueOf(raspberrySupportedVersion).MapKeys()
+	for _, value := range keys {
+		if paramVersion == reflect.Value(value).String() {
+			data, _ := json.Marshal(Pinout{Pin: raspberrySupportedVersion[paramVersion]})
+			fmt.Println(data)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(data)
+		}
+	}
 }
 
 // On function use for feed the corrent.
@@ -95,6 +114,7 @@ func main() {
 	defer rpio.Close()
 
 	router := mux.NewRouter()
+	router.HandleFunc("/version/", Version).Methods("GET")
 	router.HandleFunc("/on/", On).Methods("GET")
 	router.HandleFunc("/off/", Off).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
